9aaea745f15f2e748f2aa2c56a9400477578455d
marshal <marshal.wu@qq.com>
2020-02-08 21:32:57 +0800

    add exit code

--- spider/simple/app.js
+++ spider/simple/app.js
@@ -3,7 +3,12 @@ import yaml from 'js-yaml'
 import fs from 'fs'
 
 const config = yaml.safeLoad(fs.readFileSync('../../config/spider.yml', 'utf8'))
-new Spider(config).start()
+
+new Spider(config).start().then(() => {
+    process.exit(0)
+})
+
+
 
 
 
